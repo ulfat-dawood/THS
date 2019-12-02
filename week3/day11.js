46a3f0e5b2a14ead61c45d3409be1d654031892f
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-02 19:23:55 +0530

    forEacc map difference

--- week3/day11.js
+++ week3/day11.js
@@ -3,8 +3,18 @@ var arr= [0,1,2,3,4];
 
 //forEach
     var x= (o)=>{
-        //console.log(o);
+         o+1;
     }
 arr.forEach(x)
 
+
 //map   
+var x= (o)=>{
+    //console.log(o);
+}
+arr.map(x)
+
+//they look very similar there is a fundamental difference between forEach() and map() functions.
+
+//forEach() changes the original array, whereas map() returns a new array, without mutating the original array.
+
