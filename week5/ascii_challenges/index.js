03ee72cbb99e8a45508e31cdca32479d66fa622f
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-09 13:59:43 +0530

    ASCII challege

--- week5/ascii_challenges/index.js
+++ week5/ascii_challenges/index.js
@@ -1,6 +1,6 @@
 var readlineSync = require("readline-sync");
 
-var input = readlineSync.question("enter you first name?");
+var input = readlineSync.question("enter you first name? ");
 
 var nameArr= input.split("");
 
@@ -11,4 +11,20 @@ for(var i=0;i<nameArr.length;i++){
     asciiArr.push(ascii[nameArr[i]]);
 }
 
-console.log(asciiArr);
+const decToBi = num => num === 0 ? 0 : num % 2 + 10 * decToBi(Math.floor(num / 2));
+
+binArr =[];
+for(var i=0;i<asciiArr.length;i++){
+    binArr.push(decToBi(asciiArr[i]));
+}
+padArr=[];
+for(var i=0;i<binArr.length;i++){
+    var number = binArr[i];
+    while(number.toString().length<8){
+        number = "0" + number;
+    }
+    padArr.push(number);
+}
+
+console.log(padArr);
+
---
+++ week5/ascii_challenges/test.js
@@ -0,0 +1,7 @@
+var number= 1010;
+
+
+while(number.toString().length<8){
+    number = "0" + number;
+}
+console.log(number);
