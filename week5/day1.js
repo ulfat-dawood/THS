42003c0f641bc895f29a3612e774abec5af484c7
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-10 09:37:03 +0530

    day1

---
+++ week5/day1.js
@@ -0,0 +1,15 @@
+// var a;
+// var b= "";
+// if(b)
+// console.log("ture");
+// else
+// console.log("false");
+
+
+var obj={};
+obj.firstName="hi";
+obj[1]="world";
+console.log(obj.firstName);
+console.log(obj[1]);
+console.log(obj[firstName]);
+
