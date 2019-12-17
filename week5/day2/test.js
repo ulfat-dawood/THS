cf18fb0160b7d25cc497d42e159214334c2cd1a6
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-17 22:29:03 +0530

    mind sweeper final

--- week5/day2/test.js
+++ week5/day2/test.js
@@ -1,12 +1,25 @@
-var table="";
-var value;
-    for(var j=0; j<=90; j=j+10){ 
-        table+= "<tr>";
-        for(var i=1; i<=10;i++){
-            var value= i+j;
-            table+= "<td>"+value+"</td> ";
-        }
-        table+="</tr>\n";
+// var table="";
+// var value;
+//     for(var j=0; j<=90; j=j+10){ 
+//         table+= "<tr>";
+//         for(var i=1; i<=10;i++){
+//             var value= i+j;
+//             table+= "<td>"+value+"</td> ";
+//         }
+//         table+="</tr>\n";
         
-    } 
-console.log(table);
+//     } 
+// console.log(table);
+
+var selected = 10;
+var x;
+for(var i =1; i<=100; i++){
+    if(multiples>99)
+    break; 
+    multiples=(selected*i);
+    document.getElementById(multiples+"").innerHTML="*";
+   
+ 
+}
+
+
