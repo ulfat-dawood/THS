e9ebb9f6f96ee01f12a1bb911a8cc75281d4ffce
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-18 11:46:08 +0530

    oops

---
+++ week5/day3/oops.js
@@ -0,0 +1,17 @@
+function Person(first, last, age, gender, interests) {
+    this.firstname= first;
+    this.lastname= last;
+    this.age= age;
+    this.gender;
+    this.interests;
+    this.bio= function(){
+        console.log(this.firstname+this.lastname+this.age+this.gender);
+    }
+}
+
+Person.prototype.hobby =()=>{
+    console.log(this.interests +"I can access all properties");
+}
+Person.prototype.setAge=(age)=>{
+    this.age=age; 
+}
