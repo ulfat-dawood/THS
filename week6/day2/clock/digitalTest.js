19e7a06c95c965d6184e02b29c63d4ac482d87bd
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-25 10:16:10 +0530

    font added

--- week6/day2/clock/digitalTest.js
+++ week6/day2/clock/digitalTest.js
@@ -12,20 +12,17 @@ console.log(hr+":"+min+":"+sec+" "+ampm );
 
 var time=()=>{
     var tim = new Date();
-var min = tim.getMinutes();
-var hr = tim.getHours();
-var sec =tim.getSeconds();
-var ampm= "am"; 
+    var min = tim.getMinutes();
+    var hr = tim.getHours();
+    var sec =tim.getSeconds();
+    var ampm= "am"; 
 
-if(hr>12){
-    hr=hr-12;
-    ampm="pm";
+    if(hr>12){
+        hr=hr-12;
+        ampm="pm";
+    }
+    console.log(hr+":"+min+":"+sec+" "+ampm ); 
+
+        setTimeout(time,1000);
 }
-console.log(hr+":"+min+":"+sec+" "+ampm ); 
-
-    setTimeout(time,1000);
-}
-
-
-
- 
+time();
