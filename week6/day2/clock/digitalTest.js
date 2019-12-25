cd7a3e72ee0d81b747e98f57371febd6d639a781
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-25 18:51:49 +0530

    clock

--- week6/day2/clock/digitalTest.js
+++ week6/day2/clock/digitalTest.js
@@ -1,18 +1,53 @@
 
+var tim= new Date(); 
+console.log(tim);
 
-var time=()=>{
-    var tim = new Date();
-    var min = tim.getMinutes();
-    var hr = tim.getHours();
-    var sec =tim.getSeconds();
-    var ampm= "am"; 
-
-    if(hr>12){
-        hr=hr-12;
-        ampm="pm";
-    }
-    console.log(hr+":"+min+":"+sec+" "+ampm ); 
-
-        setTimeout(time,1000);
+for(i=0;i<5;i++){
+var tim =tim-1000;
+console.log(new Date(tim));
 }
-time();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var time=()=>{
+//     var tim = new Date();
+//     var min = tim.getMinutes();
+//     var hr = tim.getHours();
+//     var sec =tim.getSeconds();
+//     var ampm= "am"; 
+
+//     if(hr>12){
+//         hr=hr-12;
+//         ampm="pm";
+//     }
+//     console.log(hr+":"+min+":"+sec+" "+ampm ); 
+
+//         setTimeout(time,1000);
+// }
+// time();
+
+
+// setInterval(() => {
+    
+// }, interval);
