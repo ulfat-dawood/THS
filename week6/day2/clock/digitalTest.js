7cbabb384a2f9853341995f1de59ed538bf310ce
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-25 15:10:53 +0530

    system task

--- week6/day2/clock/digitalTest.js
+++ week6/day2/clock/digitalTest.js
@@ -1,14 +1,4 @@
-var tim = new Date();
-var min = tim.getMinutes();
-var hr = tim.getHours();
-var sec =tim.getSeconds();
-var ampm= "am"; 
 
-if(hr>12){
-    hr=hr-12;
-    ampm="pm";
-}
-console.log(hr+":"+min+":"+sec+" "+ampm ); 
 
 var time=()=>{
     var tim = new Date();
---
+++ week6/day3/systemTaskUlfat.js
@@ -0,0 +1,60 @@
+//1) Define Abstraction ? Implement Abstraction in JS with a working code example.
+
+//hiding irrelevant details from users and only allowing them to use the funtionalities
+
+ 
+	
+//2) Define Inheritance? Implement Inheritance in JS with a working code example.
+//Inheritance is the ability to create a child of any class. That child inherits all properties of its parent  can inherit these peroperties to its children.
+//Person has two children: Employee and Students 
+	
+function Person(firstName, lastName) {
+    this.FirstName = firstName;
+    this.LastName = lastName;
+};
+
+function Employee(firstName, lastName, salary)
+{
+    Person.call(this, firstName, lastName);
+
+    this.salary = salary;
+}
+
+function Student(firstName, lastName, gpa)
+{
+    Person.call(this, firstName, lastName);
+
+    this.gpa = gpa;
+}
+var person1= new Person("ulfat","dawood");
+var student1= new Student("Mike","smith",3.5)
+
+
+//3) Define Polymorphism? Implement Polymorphism in JS with a working code example.
+
+//when any vlaue could be any of many types:
+
+function Instructor(firstName, lastName, salary, degree)
+{
+    Student.call(this, firstName, lastName,salary);
+
+    this.degree = degree;
+}
+var instructor1 = new Volunteer("jim", "robert", 3.3, 500);
+//instructor1 can be Instructor, Employee, Person or Object:
+
+console.log(instructor1 instanceof Instructor);
+console.log(instructor1 instanceof Employee);
+console.log(instructor1 instanceof Person);
+console.log(instructor1 instanceof Object);
+
+
+//4) Define Encapsulation ? Implement Encapsulation in JS with a working code example
+
+//when objects or funtion prototypes have some values that are private to them and can't be accessed. 
+//can be achived by using symboles or dropping "this" keyword 
+
+function Volunteer(firstName, lastName) {
+    this.FirstName = Symbol('firstName');
+    LastName = lastName;
+};
