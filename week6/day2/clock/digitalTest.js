b07f537f05431edb66399f13cbbc650f1273be62
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-26 09:38:03 +0530

    fix date issue

--- week6/day2/clock/digitalTest.js
+++ week6/day2/clock/digitalTest.js
@@ -1,53 +1,9 @@
+var time= (new Date()).setHours(0,0,0,0);
+var future= time+10000;
+console.log(new Date(time).toString());
 
-var tim= new Date(); 
-console.log(tim);
-
-for(i=0;i<5;i++){
-var tim =tim-1000;
-console.log(new Date(tim));
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var time=()=>{
-//     var tim = new Date();
-//     var min = tim.getMinutes();
-//     var hr = tim.getHours();
-//     var sec =tim.getSeconds();
-//     var ampm= "am"; 
-
-//     if(hr>12){
-//         hr=hr-12;
-//         ampm="pm";
-//     }
-//     console.log(hr+":"+min+":"+sec+" "+ampm ); 
-
-//         setTimeout(time,1000);
-// }
-// time();
-
-
-// setInterval(() => {
+var func=()=>{
     
-// }, interval);
+    cz(new Date(future -=1000));
+}
+setInterval(func,1000);
