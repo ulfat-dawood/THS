b25ad226c7a6a741f8dacd0fef0607388b51ee7f
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-28 19:31:12 +0530

    promise chaining

---
+++ week6/async/promise_chaining.js
@@ -0,0 +1,13 @@
+var sum= (num1,num2)=>{
+    return new Promise(function(resolve,reject){
+
+      resolve(num1+num2);
+    
+    })
+}
+
+
+sum(1,2)
+.then(function(response){
+    return response; //response is resolve object with the value of num1++num2
+}).then(function(re))
