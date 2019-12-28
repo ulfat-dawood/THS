2ddbf770f1a712eeda5e0e8f48388df91363a1b5
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-28 18:28:37 +0530

    require final

--- week6/async/require.js
+++ week6/async/require.js
@@ -1,7 +1,7 @@
 var axios = require("./axios");
 
 
-//console.log(axios);
+//valid url
 axios("url")
  .then(function(res){
      console.log(res);
@@ -9,7 +9,7 @@ axios("url")
  .catch(function(res){
      console.log(res);
  })
-
+//number url
  axios(6)
  .then(function(res){
      console.log(res);
@@ -17,5 +17,11 @@ axios("url")
  .catch(function(res){
      console.log(res);
  })
-
- 
+//invalid url
+ axios({})
+ .then(function(res){
+     console.log(res);
+ })
+ .catch(function(res){
+     console.log(res);
+ })
