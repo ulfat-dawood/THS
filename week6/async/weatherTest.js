9578e75785859783d28ae57acedc6c89bd85096b
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-29 19:08:43 +0530

    can't resolve while :(

--- week6/async/weatherTest.js
+++ week6/async/weatherTest.js
@@ -3,8 +3,9 @@ const axios = require('axios');
 const key= "bd1fbea32cf38327b8a70a24a38fc190";
 var question= `________________________
 Welcome to Weather App
-
-ENTER CITY NAME: `
+-Enter city name
+-press q to Quit
+_`
 
 let respose=-1;
 
@@ -18,8 +19,36 @@ var input=()=>{
     })
 }
 
-input()
-.then(function(url){
-   console.log(url); 
-})
+
+
+async function askMom() {
+    try {
+        
+        do{
+            let url = await input();
+            if(url == "q") break;
+            let message = await axios.get(url);
+            console.log(message);
+            var url2 = url;
+        }while(true)
+        
+
+        
+
+        
+        
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+}
+
+(async () => {
+    await askMom();
+})();
+
+// input()
+// .then(function(url){
+//    console.log(url); 
+// })
 
