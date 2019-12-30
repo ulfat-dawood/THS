48ae05023c4e0565e6a9a60f5748b4e3ac46c9a4
ulfat-dawood <ulfat_dawood@outlook.com>
2019-12-30 11:35:51 +0530

    diagonal difference

---
+++ week7/day1/matrix.js
@@ -0,0 +1,15 @@
+var n = 3; 
+ n= n-1; 
+var arr = [ [ 11, 2, 4 ], 
+            [ 4, 5, 6 ], 
+            [ 10, 8, -12 ] ];
+
+val1=0;
+val2=0
+for( i=n; i>=0; i--){
+    val1+=(arr[n-i][n-i]); 
+    val2+=(arr[n-i][i]);      
+ }
+
+console.log(Math.abs(val1-val2))
+
