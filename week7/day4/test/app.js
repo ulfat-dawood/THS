b8352ce8fdf9b5fedd4f9a8ab31de7fdf043578c
ulfat-dawood <ulfat_dawood@outlook.com>
2020-01-03 11:59:11 +0530

    commi t

--- week7/day4/test/app.js
+++ week7/day4/test/app.js
@@ -4,17 +4,20 @@ var app = express();
 
 const port = 8081;
 
+app.use(express.static('public'));
+
+
+
+app.get('/', (req, res)=>{
+    res.send("Hello world at /abc"); 
+    console.log("get success"); 
+});
+
+app.post('/abc', (req, res)=>{
+    res.send("post request at /abc"); 
+    console.log("post success"); 
+});    
 
 app.listen(port, ()=>{
     console.log(`Server started at ${port}`);
-});
-
-app.get('/', (req, res)=>{
-    res.send("Hello world"); 
-    console.log("success"); 
-});
-
-app.post('/', (req, res)=>{
-    res.send("post request"); 
-    console.log("success"); 
 });
