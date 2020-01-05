d54fb15ffe51759bfd3a0809847a86e79716dcbc
ulfat-dawood <ulfat_dawood@outlook.com>
2020-01-05 13:06:07 +0530

    new project

--- week7/day4/test/app.js
+++ week7/day4/test/app.js
@@ -1,22 +1,27 @@
 
 var express = require('express');
+//var bodyParser = require('body-parser'); 
+ 
 var app = express();
 
 const port = 8081;
 
 app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: false }))
 
 
+
+// // app.get('
+//  ', (req, res)=>{
+// //     res.send("Hello world at /abc"); 
+// //     console.log("get success"); 
+// // });
 
 app.get('/', (req, res)=>{
-    res.send("Hello world at /abc"); 
-    console.log("get success"); 
+    console.log("done"); 
+    res.sendFile("/Users/ulfat_dawood/Desktop/TheHackingSchool/THS/week7/day4/test/public/index.html"); 
+    //res.send("done")
 });
-
-app.post('/abc', (req, res)=>{
-    res.send("post request at /abc"); 
-    console.log("post success"); 
-});    
 
 app.listen(port, ()=>{
     console.log(`Server started at ${port}`);
---
+++ week7/day4/test/test/test.js
@@ -0,0 +1,13 @@
+var express= require('express');
+var app= express();
+var bodyParser= require('body-parser'); 
+
+var port= "1234";
+
+// app.use(express.static('views'));
+
+// app.listen(port,()=>{
+//     console.log(`server started at ${port}`);
+// })
+
+console.log(__dirname)
